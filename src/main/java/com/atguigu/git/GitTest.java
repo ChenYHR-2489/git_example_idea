d3075d5399bb9d93af835740a37f5927b9a6a8a8
CYHR <1043667569@qq.com>
2023-07-01 20:16:47 +0800

    hot-fix commit time 2，测试一下冲突合并

--- src/main/java/com/atguigu/git/GitTest.java
+++ src/main/java/com/atguigu/git/GitTest.java
@@ -6,5 +6,6 @@ public class GitTest {
         System.out.println("hello git修改，目录会变成蓝色，代表追踪文件被修改！");
         System.out.println("hello git第三次修改，本次体验的是直接从项目目录右键commit,跳过add操作");
         System.out.println("hello git4 from hot-fix");
+        System.out.println("hello git4 from hot-fix,这条是为了冲突合并产生的！");
     }
 }
