9e49f25b8ebbdf5288c859fc55b8aa73eb5a7814
CYHR <1043667569@qq.com>
2023-07-01 20:11:19 +0800

    hot-fix commit 1

--- src/main/java/com/atguigu/git/GitTest.java
+++ src/main/java/com/atguigu/git/GitTest.java
@@ -5,5 +5,6 @@ public class GitTest {
         System.out.println("hello git！");
         System.out.println("hello git修改，目录会变成蓝色，代表追踪文件被修改！");
         System.out.println("hello git第三次修改，本次体验的是直接从项目目录右键commit,跳过add操作");
+        System.out.println("hello git4 from hot-fix");
     }
 }
