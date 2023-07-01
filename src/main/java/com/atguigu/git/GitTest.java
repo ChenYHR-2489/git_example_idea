6c0f3f6387c1c5534be4b03d6421970e41bd1855
CYHR <1043667569@qq.com>
2023-07-01 19:53:38 +0800

    third commit by idea time:2023/7/1

--- src/main/java/com/atguigu/git/GitTest.java
+++ src/main/java/com/atguigu/git/GitTest.java
@@ -4,5 +4,6 @@ public class GitTest {
     public static void main(String[] args) {
         System.out.println("hello git！");
         System.out.println("hello git修改，目录会变成蓝色，代表追踪文件被修改！");
+        System.out.println("hello git第三次修改，本次体验的是直接从项目目录右键commit,跳过add操作");
     }
 }
