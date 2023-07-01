6ffaa6af3bf01de0e65fd12fe54761cff8c9ce0b
CYHR <1043667569@qq.com>
2023-07-01 20:18:00 +0800

    master commit ，测试一下冲突合并

--- src/main/java/com/atguigu/git/GitTest.java
+++ src/main/java/com/atguigu/git/GitTest.java
@@ -6,5 +6,6 @@ public class GitTest {
         System.out.println("hello git修改，目录会变成蓝色，代表追踪文件被修改！");
         System.out.println("hello git第三次修改，本次体验的是直接从项目目录右键commit,跳过add操作");
         System.out.println("hello git4 from hot-fix");
+        System.out.println("hello git4 from master,这条也是为了测试一下合并冲突！");
     }
 }
