d6ce682f3d70fc73d544639f91a9873ec0ad1b28
CYHR <1043667569@qq.com>
2023-07-01 19:51:27 +0800

    second commit by idea time:2023/7/1

--- src/main/java/com/atguigu/git/GitTest.java
+++ src/main/java/com/atguigu/git/GitTest.java
@@ -3,5 +3,6 @@ package com.atguigu.git;
 public class GitTest {
     public static void main(String[] args) {
         System.out.println("hello git！");
+        System.out.println("hello git修改，目录会变成蓝色，代表追踪文件被修改！");
     }
 }
